977a561fc5f369d0713165f4c4769596bb44f872
tcocco20 <theo@coccoscoding.dev>
2025-04-18 15:08:42 -0700

    added small changes to test deployment

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
