2efe0ea40d4554e6ebd9ee50bacc5bf77315e56b
tcocco20 <theo@coccoscoding.dev>
2025-04-20 18:53:36 -0700

    started setting up OAuth flow, will save profile into db next

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,41 @@
+import "dotenv/config";
 import express from "express";
+import passport from "passport";
+import GoogleStrategy from "passport-google-oauth20";
 
+const Strategy = GoogleStrategy.Strategy;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ message: "Hello World!" });
-});
+passport.use(
+  new Strategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("Access Token:", accessToken);
+      console.log("Refresh Token:", refreshToken);
+      // Here you would typically save the user to your database
+      console.log("User authenticated:", profile);
+      return done(null, profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
 
 const { PORT = 3000 } = process.env;
 
