8690bb3af02b1a96fedb9b5b2bd4f5968cbc8dd3
tcocco20 <theo@coccoscoding.dev>
2025-04-21 20:48:50 -0700

    created react app inside of client folder.

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+      <p>It is using TypeScript for type safety.</p>
+      <p>Enjoy coding!</p>
+    </>
+  );
+}
+
+export default App;
