6fadc85375ada3ee9f2394fe28e48e39cdc4570b
tcocco20 <theo@coccoscoding.dev>
2025-04-24 17:33:25 -0700

    set up for deployment

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import "./services/passport.ts";
 import { authRoutes } from "./routes/authRoutes.ts";
 import cookieSession from "cookie-session";
 import passport from "passport";
-import { COOKIE_KEY, PORT } from "./config/constants.ts";
+import { COOKIE_KEY, ENVIRONMENT, PORT } from "./config/constants.ts";
 import { todoRoutes } from "./routes/todoRoutes.ts";
 
 const app = express();
@@ -20,6 +20,14 @@ app.use(express.json());
 
 authRoutes(app);
 todoRoutes(app);
+
+if (ENVIRONMENT === "production") {
+  app.use(express.static("client/dist"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
