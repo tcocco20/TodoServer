4e2b2ddcc84488d5ca71a32d66e418ad259162f3
tcocco20 <theo@coccoscoding.dev>
2025-04-24 17:42:40 -0700

    moved path import outside of if statement

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import "./services/passport.ts";
 import { authRoutes } from "./routes/authRoutes.ts";
 import cookieSession from "cookie-session";
 import passport from "passport";
+import path from "path";
 import { COOKIE_KEY, ENVIRONMENT, PORT } from "./config/constants.ts";
 import { todoRoutes } from "./routes/todoRoutes.ts";
 
@@ -23,7 +24,6 @@ todoRoutes(app);
 
 if (ENVIRONMENT === "production") {
   app.use(express.static("client/dist"));
-  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
